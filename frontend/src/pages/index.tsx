8fab39f3fdc252a9ba2d69f76cd5028d76b2d721
Igor Papandinas <igor.papandinas@posteo.net>
2023-11-26 14:32:31 +0100

    fix: build works

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,7 +3,6 @@ import { HomeTopBar } from '@/components/home/HomeTopBar'
 import { CenterBody } from '@/components/layout/CenterBody'
 import { ChainInfo } from '@/components/web3/ChainInfo'
 import { ConnectButton } from '@/components/web3/ConnectButton'
-import { GreeterContractInteractions } from '@/components/web3/GreeterContractInteractions'
 import { useInkathon } from '@scio-labs/use-inkathon'
 import type { NextPage } from 'next'
 import { useEffect } from 'react'
@@ -33,9 +32,6 @@ const HomePage: NextPage = () => {
         <div tw="mt-10 flex w-full flex-wrap items-start justify-center gap-4">
           {/* Chain Metadata Information */}
           <ChainInfo />
-
-          {/* Greeter Read/Write Contract Interactions */}
-          <GreeterContractInteractions />
         </div>
       </CenterBody>
     </>
